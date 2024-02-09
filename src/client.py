48c700c1dc99cdcd141708c2fea63287837ca5d4
Connor <connor.york000@gmail.com>
2024-02-09 11:08:37 +0000

    Basic functionality implemented

---
+++ src/client.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import urllib.request
+import json
+
+url = 'http://localhost:8000'  # Change the URL if the server runs on a different machine or port
+
+data_request = {
+    'name': 'Client',
+    'request': True,                                                                                                                              
+}
+
+data_update = {
+    'name': 'Client',
+    'request': False,
+    'rewards': [1,2,3,4],
+    'no_rewards': [5,6,7,8]
+}
+
+user_input = input("Update or Req: [u or r] ")
+
+if user_input == 'u':
+    data_update = json.dumps(data_update).encode('utf-8')
+    req = urllib.request.Request(url, data=data_update, headers={'Content-Type': 'application/json'})
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8')) 
+else: 
+    data_request = json.dumps(data_request).encode('utf-8')
+    req = urllib.request.Request(url, data=data_request, headers={'Content-Type': 'application/json'})
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
---
+++ src/server.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+reward_IDs = []
+no_reward_IDs = []
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        #print(data)
+        self.end_headers()
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = json.loads(post_data.decode('utf-8'))
+        self._set_response()
+
+        if data["request"]: # MESSAGE BACK THE LIST OF REWARDS AND NOT REWARDS
+            print("This is a data request")
+            response = {
+                'name': 'Server',
+                'reward': reward_IDs,
+                'no_reward': no_reward_IDs
+            }
+            response = json.dumps(response).encode('utf-8')
+            self.wfile.write(response)
+        else:
+            print("This is an update")
+            for id in data["rewards"]:
+                if id not in reward_IDs:
+                    reward_IDs.append(id)
+                if id in no_reward_IDs:
+                    no_reward_IDs.remove(id)
+            for id in data["no_rewards"]:
+                if id not in no_reward_IDs:
+                    no_reward_IDs.append(id)
+                if id in reward_IDs:
+                    reward_IDs.remove(id)
+            self.wfile.write("Update received".encode('utf-8'))
+            
+
+
+
+        #self.wfile.write("Data received by the server:\n".encode())
+        #self.wfile.write(json.dumps(data).encode())
+
+    def send_message(self,message):
+        print("test lol")
+        self.wfile.write(message.encode())
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Server started on port {port}")
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run()
