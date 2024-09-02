137feb89f5e48e05a008a9e1f90643de54893109
Connor-York <connor.york000@gmail.com>
2024-09-02 17:49:21 +0100

    moved old to old, rewrote server using Flask, rewrote rosclient as rosclient2.py

--- src/FlaskServer.py
+++ src/FlaskServer.py
@@ -2,15 +2,41 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+server_url_local = '10.0.0.131'
+server_url = '192.168.1.100'
+
+#clients:
+connor = "192.168.1.101"
+mehdi = "192.168.1.102"
+jay = "192.168.1.103"
+james = "192.168.1.104"
+clients = [connor,mehdi,jay,james]
+
 # Endpoint to receive JSON data
 @app.route('/receive', methods=['POST'])
 def receive_json():
     data = request.get_json()  # Get the JSON data from the request
     print(f"Received JSON: {data}")
 
+    if data.target = None:
+        respond_all(data)
+    else:
+        response = requests.post(data.target, json=data)
+        print(f"Sent data to {data.target}, response status: {response.status_code}")
+
     # Send a response back
     response = {"message": "JSON received successfully!", "data_received": data}
     return jsonify(response)
 
+def respond_all(data):
+
+    for client_url in clients
+        try:
+            response = requests.post(client_url, json=data)
+            print(f"Sent data to {client_url}, response status: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to send data to {client_url}: {e}")
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host=server_url_local, port=5000)
--- src/rosclient2.py
+++ src/rosclient2.py
@@ -1,17 +1,34 @@
 import requests
+import rospy
+from std_msgs.msg import String
+import json
 
-# URL of the server endpoint
-url = 'http://127.0.0.1:5000/receive'
+# local network
+# server_url = 'http://10.0.0.131:5000/receive'
 
-# JSON data to send
-json_data = {
-    "name": "John Doe",
-    "age": 30,
-    "city": "New York"
-}
+# robot_5 network
+server_url = 'http://192.168.1.100:5000/receive
 
-# Send POST request with JSON data
-response = requests.post(url, json=json_data)
 
-# Print the response from the server
-print(f"Response from server: {response.json()}")
+def callback(data):
+    message = data.data # already json, might need a 
+
+    # Send POST request with JSON data
+    response = requests.post(server_url, json=message)
+
+    # Print the response from the server
+    print(f"Response from server: {response.json()}")
+
+
+
+
+def message_passer():
+    rospy.init_node('message_passer', anonymous=True)
+    rospy.Subscriber('/server_pub', String, callback)
+    print("Client-side Initialised")
+    rospy.spin()
+
+
+
+if __name__ == '__main__':
+    message_passer()
