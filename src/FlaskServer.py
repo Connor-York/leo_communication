1b73354c555515da73e3379ba2560fd9cbac6b90
Connor <connor.york000@gmail.com>
2024-09-04 14:16:43 +0100

    updated FlaskServer.py

--- src/FlaskServer.py
+++ src/FlaskServer.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -6,23 +7,27 @@ server_url_local = '10.0.0.131'
 server_url = '192.168.1.100'
 
 #clients:
-connor = "192.168.1.101"
-mehdi = "192.168.1.102"
-jay = "192.168.1.103"
-james = "192.168.1.104"
+connor = "http://192.168.1.101:5000/receive"
+mehdi = "http://192.168.1.102:5000/receive"
+jay = "http://192.168.1.103:5000/receive"
+james = "http://192.168.1.104:5000/receive"
 clients = [connor,mehdi,jay,james]
+
+
 
 # Endpoint to receive JSON data
 @app.route('/receive', methods=['POST'])
 def receive_json():
     data = request.get_json()  # Get the JSON data from the request
     print(f"Received JSON: {data}")
-
-    if data.target = None:
+    print("Data type:")
+    print(type(data))
+    if data["targets"] == None:
         respond_all(data)
     else:
-        response = requests.post(clients[data.target], json=data)
-        print(f"Sent data to {clients[data.target]}, response status: {response.status_code}")
+        target = data["targets"]
+        response = requests.post(clients[target], json=data)
+        print(f"Sent data to {clients[target]}, response status: {response.status_code}")
 
     # Send a response back
     response = {"message": "JSON received successfully!", "data_received": data}
@@ -30,7 +35,7 @@ def receive_json():
 
 def respond_all(data):
 
-    for client_url in clients
+    for client_url in clients:
         try:
             response = requests.post(client_url, json=data)
             print(f"Sent data to {client_url}, response status: {response.status_code}")
@@ -39,4 +44,4 @@ def respond_all(data):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host=server_url_local, port=5000)
+    app.run(debug=True, host=server_url, port=5000)
