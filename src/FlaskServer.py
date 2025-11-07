61ab102dd67dda11268e26536566870f3e354b81
Connor-York <connor.york000@gmail.com>
2025-11-07 14:36:19 +0000

    Update prior to testing

--- src/FlaskServer.py
+++ src/FlaskServer.py
@@ -7,11 +7,12 @@ server_url_local = '10.0.0.131'
 server_url = '192.168.1.100'
 
 #clients:
-connor = "http://192.168.1.101:5000/receive"
+#connor = "http://192.168.1.101:5000/receive"
+connor = "http://10.0.0.1:5000/receive"
 mehdi = "http://192.168.1.102:5000/receive"
 jay = "http://192.168.1.103:5000/receive"
 james = "http://192.168.1.104:5000/receive"
-clients = [connor,mehdi,jay,james]
+clients = [connor] #,mehdi,jay,james]
 
 
 
@@ -22,12 +23,13 @@ def receive_json():
     print(f"Received JSON: {data}")
     print("Data type:")
     print(type(data))
-    if data["targets"] == None:
-        respond_all(data)
-    else:
-        target = data["targets"]
-        response = requests.post(clients[target], json=data)
-        print(f"Sent data to {clients[target]}, response status: {response.status_code}")
+    respond_all(data)
+    # if data["targets"] == None:
+    #     respond_all(data)
+    # else:
+    #     target = data["targets"]
+    #     response = requests.post(clients[target], json=data)
+    #     print(f"Sent data to {clients[target]}, response status: {response.status_code}")
 
     # Send a response back
     response = {"message": "JSON received successfully!", "data_received": data}
@@ -44,4 +46,4 @@ def respond_all(data):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host=server_url, port=5000)
+    app.run(debug=True, host=server_url_local, port=5000)
